f69d8a4f5ce77100f898305cbca5fe55a527a1b7
Konstantin Willeke <konstantin.willeke@gmail.com>
2019-12-19 16:42:40 +0100

    Add Gaussian Readout and Add Depth-Separable-Convs.

--- nnfabrik/models/gaussian_readout_models.py
+++ nnfabrik/models/gaussian_readout_models.py
@@ -10,6 +10,7 @@ from mlutils.constraints import positive
 from mlutils.layers.cores import DepthSeparableConv2d, Core2d, Stacked2dCore
 from ..utility.nn_helpers import get_io_dims, get_module_output, set_random_seed, get_dims_for_loader_dict
 from mlutils import regularizers
+from mlutils.layers.readouts import PointPooled2d
 
 
 class DepthSeparableCore(Core2d, nn.Module):
@@ -127,6 +128,34 @@ class DepthSeparableCore(Core2d, nn.Module):
         return len(self.features) * self.hidden_channels
 
 
+class PointPooled2dReadout(nn.ModuleDict):
+    def __init__(self, core, in_shape_dict, n_neurons_dict, pool_steps, pool_kern, bias, init_range, gamma_readout):
+        # super init to get the _module attribute
+        super(PointPooled2dReadout, self).__init__()
+        for k in n_neurons_dict:
+            in_shape = get_module_output(core, in_shape_dict[k])[1:]
+            n_neurons = n_neurons_dict[k]
+            self.add_module(k, PointPooled2d(
+                            in_shape,
+                            n_neurons,
+                            pool_steps=pool_steps,
+                            pool_kern=pool_kern,
+                            bias=bias,
+                            init_range=init_range)
+                            )
+
+        self.gamma_readout = gamma_readout
+
+    def forward(self, *args, data_key=None, **kwargs):
+        if data_key is None and len(self) == 1:
+            data_key = list(self.keys())[0]
+        return self[data_key](*args, **kwargs)
+
+
+    def regularizer(self, data_key):
+        return self[data_key].feature_l1(average=False) * self.gamma_readout
+
+
 class MultiGaussReadout(nn.ModuleDict):
     def __init__(self, core, in_shape_dict, n_neurons_dict, init_mu_range, init_sigma_range, bias, gamma_readout):
         # super init to get the _module attribute
@@ -154,12 +183,12 @@ class MultiGaussReadout(nn.ModuleDict):
 
 
 def ds_core_gauss_readout(dataloaders, seed, hidden_channels=32, input_kern=13,          # core args
-                                 hidden_kern=3, layers=3, gamma_hidden=0, gamma_input=0.1,
-                                 skip=0, final_nonlinearity=True, core_bias=False, momentum=0.9,
+                                 hidden_kern=3, layers=3,  gamma_input=0.1,
+                                 skip=0, final_nonlinearity=True, momentum=0.9,
                                  pad_input=False, batch_norm=True, hidden_dilation=1,
                                  laplace_padding=None, input_regularizer='LaplaceL2norm',
                                  init_mu_range=0.2, init_sigma_range=0.5, readout_bias=True,  # readout args,
-                                 gamma_readout=0.1,  elu_offset=0, stack=None,
+                                 gamma_readout=4,  elu_offset=0, stack=None,
                                  ):
     """
     Model class of a stacked2dCore (from mlutils) and a pointpooled (spatial transformer) readout
@@ -235,6 +264,177 @@ def ds_core_gauss_readout(dataloaders, seed, hidden_channels=32, input_kern=13, 
     model = Encoder(core, readout, elu_offset)
 
     return model
+
+
+def ds_core_point_readout(dataloaders, seed, hidden_channels=32, input_kern=13,          # core args
+                                 hidden_kern=3, layers=3, gamma_input=0.1,
+                                 skip=0, final_nonlinearity=True, core_bias=False, momentum=0.9,
+                                 pad_input=False, batch_norm=True, hidden_dilation=1,
+                                 laplace_padding=None, input_regularizer='LaplaceL2norm',
+                                 pool_steps=2, pool_kern=3, readout_bias=True,  # readout args,
+                                 init_range=0.2, gamma_readout=0.1,  elu_offset=0, stack=None,
+                                 ):
+    """
+    Model class of a stacked2dCore (from mlutils) and a pointpooled (spatial transformer) readout
+
+    Args:
+        dataloaders: a dictionary of dataloaders, one loader per session
+            in the format {'data_key': dataloader object, .. }
+        seed: random seed
+        elu_offset: Offset for the output non-linearity [F.elu(x + self.offset)]
+
+        all other args: See Documentation of Stacked2dCore in mlutils.layers.cores and
+            PointPooled2D in mlutils.layers.readouts
+
+    Returns: An initialized model which consists of model.core and model.readout
+    """
+
+    if "train" in dataloaders.keys():
+        dataloaders = dataloaders["train"]
+
+    session_shape_dict = get_dims_for_loader_dict(dataloaders)
+    n_neurons_dict = {k: v['targets'][1] for k, v in session_shape_dict.items()}
+    in_shapes_dict = {k: v['inputs'] for k, v in session_shape_dict.items()}
+    input_channels = [v['inputs'][1] for _, v in session_shape_dict.items()]
+    assert np.unique(input_channels).size == 1, "all input channels must be of equal size"
+
+    class Encoder(nn.Module):
+
+        def __init__(self, core, readout, elu_offset):
+            super().__init__()
+            self.core = core
+            self.readout = readout
+            self.offset = elu_offset
+
+        def forward(self, x, data_key=None, **kwargs):
+            x = self.core(x)
+            x = self.readout(x, data_key=data_key)
+            return F.elu(x + self.offset) + 1
+
+        def regularizer(self, data_key):
+            return self.core.regularizer() + self.readout.regularizer(data_key=data_key)
+
+    set_random_seed(seed)
+
+    # get a stacked2D core from mlutils
+    core = DepthSeparableCore(input_channels=input_channels[0],
+                         hidden_channels=hidden_channels,
+                         input_kern=input_kern,
+                         hidden_kern=hidden_kern,
+                         layers=layers,
+                         gamma_input=gamma_input,
+                         skip=skip,
+                         final_nonlinearity=final_nonlinearity,
+                         bias=core_bias,
+                         momentum=momentum,
+                         pad_input=pad_input,
+                         batch_norm=batch_norm,
+                         hidden_dilation=hidden_dilation,
+                         laplace_padding=laplace_padding,
+                         input_regularizer=input_regularizer,
+                         stack=stack)
+
+    readout = PointPooled2dReadout(core, in_shape_dict=in_shapes_dict,
+                                   n_neurons_dict=n_neurons_dict,
+                                   pool_steps=pool_steps,
+                                   pool_kern=pool_kern,
+                                   bias=readout_bias,
+                                   gamma_readout=gamma_readout,
+                                   init_range=init_range)
+
+    # initializing readout bias to mean response
+    for k in dataloaders:
+        readout[k].bias.data = dataloaders[k].dataset[:].targets.mean(0)
+
+    model = Encoder(core, readout, elu_offset)
+
+    return model
+
+
+def stacked2d_core_gaussian_readout(dataloaders, seed, hidden_channels=32, input_kern=13,          # core args
+                                 hidden_kern=3, layers=3, gamma_hidden=0, gamma_input=0.1,
+                                 skip=0, final_nonlinearity=True, core_bias=False, momentum=0.9,
+                                 pad_input=False, batch_norm=True, hidden_dilation=1,
+                                 laplace_padding=None, input_regularizer='LaplaceL2norm',
+                                 readout_bias=True, init_mu_range=0.2, init_sigma_range=0.5,  # readout args,
+                                 gamma_readout=0.1,  elu_offset=0, stack=None,
+                                 ):
+    """
+    Model class of a stacked2dCore (from mlutils) and a pointpooled (spatial transformer) readout
+
+    Args:
+        dataloaders: a dictionary of dataloaders, one loader per session
+            in the format {'data_key': dataloader object, .. }
+        seed: random seed
+        elu_offset: Offset for the output non-linearity [F.elu(x + self.offset)]
+
+        all other args: See Documentation of Stacked2dCore in mlutils.layers.cores and
+            PointPooled2D in mlutils.layers.readouts
+
+    Returns: An initialized model which consists of model.core and model.readout
+    """
+
+    if "train" in dataloaders.keys():
+        dataloaders = dataloaders["train"]
+
+    session_shape_dict = get_dims_for_loader_dict(dataloaders)
+    n_neurons_dict = {k: v['targets'][1] for k, v in session_shape_dict.items()}
+    in_shapes_dict = {k: v['inputs'] for k, v in session_shape_dict.items()}
+    input_channels = [v['inputs'][1] for _, v in session_shape_dict.items()]
+    assert np.unique(input_channels).size == 1, "all input channels must be of equal size"
+
+    class Encoder(nn.Module):
+
+        def __init__(self, core, readout, elu_offset):
+            super().__init__()
+            self.core = core
+            self.readout = readout
+            self.offset = elu_offset
+
+        def forward(self, x, data_key=None, **kwargs):
+            x = self.core(x)
+            x = self.readout(x, data_key=data_key)
+            return F.elu(x + self.offset) + 1
+
+        def regularizer(self, data_key):
+            return self.core.regularizer() + self.readout.regularizer(data_key=data_key)
+
+    set_random_seed(seed)
+
+    # get a stacked2D core from mlutils
+    core = Stacked2dCore(input_channels=input_channels[0],
+                         hidden_channels=hidden_channels,
+                         input_kern=input_kern,
+                         hidden_kern=hidden_kern,
+                         layers=layers,
+                         gamma_hidden=gamma_hidden,
+                         gamma_input=gamma_input,
+                         skip=skip,
+                         final_nonlinearity=final_nonlinearity,
+                         bias=core_bias,
+                         momentum=momentum,
+                         pad_input=pad_input,
+                         batch_norm=batch_norm,
+                         hidden_dilation=hidden_dilation,
+                         laplace_padding=laplace_padding,
+                         input_regularizer=input_regularizer,
+                         stack=stack)
+
+    readout = MultiGaussReadout(core, in_shape_dict=in_shapes_dict,
+                                   n_neurons_dict=n_neurons_dict,
+                                   init_mu_range=init_mu_range,
+                                   init_sigma_range=init_sigma_range,
+                                   bias=readout_bias,
+                                   gamma_readout=gamma_readout)
+
+    # initializing readout bias to mean response
+    for k in dataloaders:
+        readout[k].bias.data = dataloaders[k].dataset[:].targets.mean(0)
+
+    model = Encoder(core, readout, elu_offset)
+
+    return model
+
 
 class Gaussian2d(nn.Module):
     """
